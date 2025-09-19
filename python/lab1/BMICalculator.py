e33c67d9c005af1188af2597445e92de00691bb3
FOX <150899536+foxisoffline@users.noreply.github.com>
2025-09-20 02:06:31 +0800

    feat: modify bmicalculator.py

--- python/lab1/BMICalculator.py
+++ python/lab1/BMICalculator.py
@@ -1,39 +1,39 @@
-r'''
+r"""
 Task Description:
-In this task, we develop a Body Mass Index (BMI) Calculator that can be used to 
-calculate your BMI value and weight status while taking your age into 
-consideration. The referenced weight range and calculation 
+In this task, we develop a Body Mass Index (BMI) Calculator that can be used to
+calculate your BMI value and weight status while taking your age into
+consideration. The referenced weight range and calculation
 formula is listed below.
 
 The program requirement is as follows:
-1. Allow users to run your program with three input arguments by passing three 
+1. Allow users to run your program with three input arguments by passing three
    values to the program: the choice of units, height, and weight.
 
-2. Your program will read the three arguments and calculate BMI using the 
+2. Your program will read the three arguments and calculate BMI using the
    following two formulas:
     BMI = weight(kg)/height2(m2) (Metric Units)
     BMI = 703 * weight(lb)/height2(in2) (Imperial (U.S.) Units)
-    NOTE: The formulas to calculate BMI are based on two of the most used unit 
+    NOTE: The formulas to calculate BMI are based on two of the most used unit
           systems.
 
-3. After user inputs all the numbers, if the input numbers are invalid, you need 
-   to present an error message "Your input is invalid!". Otherwise, you need to 
-   print out BMI and category. The output payment requires to have 2 precisions. 
-   For instance, if BMI is 23.456, it should print 23.46. If BMI is 23, it should 
+3. After user inputs all the numbers, if the input numbers are invalid, you need
+   to present an error message "Your input is invalid!". Otherwise, you need to
+   print out BMI and category. The output payment requires to have 2 precisions.
+   For instance, if BMI is 23.456, it should print 23.46. If BMI is 23, it should
    print 23.00.
 
 Reference:
-Your BMI is a measurement of your body weight based on your height and weight. 
-Although your BMI does not actually "measure" your percentage of body fat, it is a 
-useful tool to estimate a healthy body weight based on your height. Due to its ease 
-of measurement and calculation, it is the most widely used diagnostic indicator to 
-identify a person's optimal weight depending on his height. Your BMI "number" will 
-inform you if you are underweight, of normal weight, overweight, or obese. However, 
-due to the wide variety of body types, the distribution of muscle and bone mass, etc., 
+Your BMI is a measurement of your body weight based on your height and weight.
+Although your BMI does not actually "measure" your percentage of body fat, it is a
+useful tool to estimate a healthy body weight based on your height. Due to its ease
+of measurement and calculation, it is the most widely used diagnostic indicator to
+identify a person's optimal weight depending on his height. Your BMI "number" will
+inform you if you are underweight, of normal weight, overweight, or obese. However,
+due to the wide variety of body types, the distribution of muscle and bone mass, etc.,
 it is not appropriate to use this as the only or final indication for diagnosis.
 
 BMI Table for Adults:
-This is the World Health Organization's (WHO) recommended body weight based on BMI 
+This is the World Health Organization's (WHO) recommended body weight based on BMI
 values for adults. It is used for both men and women, age 18 or older.
 ______________________________________
 |Category         | BMI range - kg/m2|
@@ -69,15 +69,49 @@ C:\INF1002\Lab1\AverageCalculator> python BMICalculator.py imperial 85.63 135.68
 
 C:\INF1002\Lab1\AverageCalculator> python BMICalculator.py abc
 Your input is invalid!
-'''
+"""
+
 import sys
+
 # You can use sys.argv[1] to get the first input argument.
 # sys.argv[2] is the second argument, etc.
 
+
 def BMICalculator():
-    pass
+
+    error = "Your input is invalid!"
+
+    cat = [
+        (16, "Severe Thinness"),
+        (17, "Moderate Thinness"),
+        (18.5, "Mild Thinness"),
+        (25, "Normal"),
+        (30, "Overweight"),
+        (35, "Obese Class I"),
+        (40, "Obese Class II"),
+        (float("inf"), "Obese Class III"),
+    ]
+
+    try:
+
+        if (len(sys.argv) != 4) or (sys.argv[1].lower() not in ["metric",
+                                                                "imperial"]):
+            raise ValueError
+
+        unit, height, weight = sys.argv[1:]
+        bmi = float(weight) / float(height) ** 2
+
+        if unit.lower() == "imperial":
+            bmi = bmi * 703
+
+        for threshold, category in cat:
+            if bmi <= threshold:
+                print(f"%0.2f\t{category}" % bmi)
+                break
+
+    except ValueError:
+        print(error)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     BMICalculator()
-    
