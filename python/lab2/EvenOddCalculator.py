0812dcba33351145544c186c2c9a610e2ce544ff
FOX <150899536+foxisoffline@users.noreply.github.com>
2025-09-24 09:51:48 +0800

    feat: modify evenoddcalculator.py

--- python/lab2/EvenOddCalculator.py
+++ python/lab2/EvenOddCalculator.py
@@ -1,22 +1,22 @@
-r'''
+r"""
 Task Description:
-In this task, you are assigned to develop one program to help users calculate 
-different information based a series of user provided numbers (integers). 
-Detailed requirement is provided as follows: 
+In this task, you are assigned to develop one program to help users calculate
+different information based a series of user provided numbers (integers).
+Detailed requirement is provided as follows:
 
-Input: A series of numbers 
+Input: A series of numbers
 Output: (Your output should be in one line)
-	. The summation of the even numbers and summation of the odd numbers in 
-       the input list 
-	. The difference between the biggest and smallest numbers in the input list
-	. The count of even numbers and odd numbers in the input list
-	. The "centered" average of the list of integers. The centered average can be 
-       calculated as the mean average of the values, after removing the largest and 
-       smallest values in the array. If there are multiple copies of the smallest 
-       value, ignore all but keep just one copy, and likewise for the largest value. 
+        . The summation of the even numbers and summation of the odd numbers in
+       the input list
+        . The difference between the biggest and smallest numbers in the input list
+        . The count of even numbers and odd numbers in the input list
+        . The "centered" average of the list of integers. The centered average can be
+       calculated as the mean average of the values, after removing the largest and
+       smallest values in the array. If there are multiple copies of the smallest
+       value, ignore all but keep just one copy, and likewise for the largest value.
        For instance, [12,2,8,7,100] -> 9; [2,2,8,11,100] -> 7
 
-To have a better understanding of the loops, please try to implement 
+To have a better understanding of the loops, please try to implement
 two programs: one uses "for" and another uses "while" loop.
 
 Running Examples:
@@ -26,8 +26,11 @@ The sum of all even numbers is 122, the sum of all odd numbers is 7, the differe
 
 C:\INF1002\Lab2\EvenOddCalculator> python EvenOddCalculator.py 1,2,abcd,8,11,200,301
 Please enter valid integers.
-'''
+"""
+
 import sys
+
+
 # write your code here
 # you can use sys.argv[1] to get the first input argument.
 # sys.argv[2] is the second argument, etc.
@@ -40,26 +43,32 @@ def EvenOddCalculator():
         if len(sys.argv) != 2:
             raise ValueError
 
-        value = sorted(list(map(int, sys.argv[1].split(sep=","))),
-                       reverse=True)
+        value = sorted(list(map(int, sys.argv[1].split(","))), reverse=True)
 
         difference = value[0] - value[-1]
 
-        even = [even for even in value if even % 2 == 0]
-        even_sum, even_len = sum(even), len(even)
+        even, odd = [], []
 
-        odd = [odd for odd in value if odd % 2 != 0]
-        odd_sum, odd_len = sum(odd), len(odd)
+        for v in value:
+            if v % 2 == 0:
+                even.append(v)
+            else:
+                odd.append(v)
+
+        even_sum, even_len, odd_sum, odd_len = sum(even), len(even), \
+            sum(odd), len(odd)
 
         average = int(sum(value[1:-1]) / len(value[1:-1]))
 
-        print(f"The sum of all even numbers is {even_sum}, "
-              f"the sum of all odd numbers is {odd_sum}, "
-              f"the difference between the biggest "
-              f"and smallest number is {difference}, "
-              f"the total number of even numbers is {even_len}, "
-              f"the total number of odd numbers is {odd_len}, "
-              f"the centered average is {average}.")
+        print(
+            f"The sum of all even numbers is {even_sum}, "
+            f"the sum of all odd numbers is {odd_sum}, "
+            f"the difference between the biggest "
+            f"and smallest number is {difference}, "
+            f"the total number of even numbers is {even_len}, "
+            f"the total number of odd numbers is {odd_len}, "
+            f"the centered average is {average}."
+        )
 
     except ValueError:
         print(error)
